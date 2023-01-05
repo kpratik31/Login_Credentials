bb1046b8071f216415252851f9256bf191f5564d
Pratik <pratikkhendake31082001@gmail.com>
2023-01-05 22:16:24 +0530

    UserLogin Webpage

---
+++ user.php
@@ -0,0 +1,31 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $db = "logincredentails";
+
+    // connection
+    $connection = mysqli_connect($servername, $username, $password, $db);
+    if(!$connection){
+        die("failed to connect".mysqli_connect_error());
+    }
+    else{
+        echo "Records Successfully";
+    }
+
+    $name = $_POST['nm'];
+    $email = $_POST['em'];
+    //insertion
+    $sql = "INSERT INTO `logindata`(`Name`,`Email`) VALUES('$name','$email')";
+
+    mysqli_query($connection,$sql);
+
+    //connection closed
+    mysqli_close($connection);
+
+
+?>
+
+
+
+
